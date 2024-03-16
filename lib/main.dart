2ffe4d0a7bfbeb515819628fa6edcb467b67def2
fercguerra <fercguerra06@gmail.com>
2024-03-16 19:48:47 -0300

    Aula 31

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 
 
-main() => runApp(PerguntaApp());
+main() {
+  runApp(PerguntaApp());
+}
 
 class PerguntaApp extends StatelessWidget {
+  const PerguntaApp({super.key});
+
 
   @override
   Widget build (BuildContext context) {
-    return const MaterialApp(
-      home: Text ('Olá Flutter'),
+    return  MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+         body: Text('Olá Flutter'),
+      ),
     );
   }
-
 }
+   
+
